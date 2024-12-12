aacec715068a734507b2eeec3d059ff07d012685
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2024-12-12 07:51:45 +0700

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -14,9 +14,6 @@ python train.py \
 --use_linear --head cos-span \
 --train_type TRADES-cos \
 --suffix 1
-
-########## TEST - CIFAR100FS ##########
-
 ### Conv4-512-CIFAR100FS-FGSM
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -31,6 +28,17 @@ python train.py \
 --eval --load_best \
 --attack FGSM
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ### Conv4-512-CIFAR100FS-AA
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -45,6 +53,17 @@ python train.py \
 --eval --load_best \
 --attack AA --n_test 100
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ### Conv4-512-CIFAR100FS-PGD
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -59,6 +78,17 @@ python train.py \
 --eval --load_best \
 --attack PGD --n_test 100
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ###  Conv4-512-CIFAR100FS-CW
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -73,6 +103,17 @@ python train.py \
 --eval --load_best \
 --attack CW --n_test 100
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ### ResNet12-CIFAR100FS-FGSM
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -87,6 +128,17 @@ python train.py \
 --eval --load_best \
 --attack FGSM
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ### ResNet12-CIFAR100FS-AA
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -101,6 +153,17 @@ python train.py \
 --eval --load_best \
 --attack AA --n_test 100
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ### ResNet12-CIFAR100FS-PGD
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -115,6 +178,17 @@ python train.py \
 --eval --load_best \
 --attack PGD --n_test 100
 
+########## TRAIN ##########
+python train.py \
+--data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+--exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+--seed 3407 \
+--n_support 0 \
+--dataset CIFAR100FS \
+--model Conv4-512 \
+--use_linear --head cos-span \
+--train_type TRADES-cos \
+--suffix 1
 ###  ResNet12-CIFAR100FS-CW
 python train.py \
 --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
@@ -129,116 +203,116 @@ python train.py \
 --eval --load_best \
 --attack CW --n_test 100
 
-########## TEST - miniImageNet ##########
+# ########## TEST - miniImageNet ##########
 
-### Conv4-512-miniImageNet-FGSM
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model Conv4-512 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack FGSM
+# ### Conv4-512-miniImageNet-FGSM
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model Conv4-512 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack FGSM
 
-### Conv4-512-miniImageNet-AA
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model Conv4-512 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack AA --n_test 100
+# ### Conv4-512-miniImageNet-AA
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model Conv4-512 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack AA --n_test 100
 
-### Conv4-512-miniImageNet-PGD
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model Conv4-512 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack PGD --n_test 100
+# ### Conv4-512-miniImageNet-PGD
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model Conv4-512 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack PGD --n_test 100
 
-###  Conv4-512-miniImageNet-CW
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model Conv4-512 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack CW --n_test 100
+# ###  Conv4-512-miniImageNet-CW
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model Conv4-512 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack CW --n_test 100
 
-### ResNet12-miniImageNet-FGSM
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model ResNet12 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack FGSM
+# ### ResNet12-miniImageNet-FGSM
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model ResNet12 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack FGSM
 
-### ResNet12-miniImageNet-AA
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model ResNet12 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack AA --n_test 100
+# ### ResNet12-miniImageNet-AA
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model ResNet12 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack AA --n_test 100
 
-### ResNet12-miniImageNet-PGD
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model ResNet12 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack PGD --n_test 100
+# ### ResNet12-miniImageNet-PGD
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model ResNet12 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack PGD --n_test 100
 
-###  ResNet12-miniImageNet-CW
-python train.py \
---data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
---exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
---seed 3407 \
---n_support 0 \
---dataset miniImageNet \
---model ResNet12 \
---use_linear --head cos-span \
---train_type TRADES-cos \
---suffix 1 \
---eval --load_best \
---attack CW --n_test 100
+# ###  ResNet12-miniImageNet-CW
+# python train.py \
+# --data_dir /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Dataset \
+# --exp_name /home/khoahocmaytinh2022/Desktop/KhanhDang/LAAT_Output \
+# --seed 3407 \
+# --n_support 0 \
+# --dataset miniImageNet \
+# --model ResNet12 \
+# --use_linear --head cos-span \
+# --train_type TRADES-cos \
+# --suffix 1 \
+# --eval --load_best \
+# --attack CW --n_test 100
